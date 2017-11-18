2ed3b10bc8aba2e191dbd171adeb972a66ac6337
ATW Default User <adtop@ATW-42-2-Master.abb.com>
2017-11-18 05:32:47 +0100

    Working first version

---
+++ server/main.js
@@ -0,0 +1,8 @@
+S3.config = {
+	key: 'AKIAIQFDWYMG7SI2CCVA',
+	secret: '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',
+	bucket: 'kjimagedb',
+	region: 'us-west-2' // Only needed if not "us-east-1" or "us-standard"
+};
+
+
